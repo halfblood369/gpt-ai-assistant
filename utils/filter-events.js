04372d6059bb266e759e82346eecc295ff5b79a7
halfblood369 <halfblood369@gmail.com>
2024-08-19 16:37:38 +0800

    构成对象 Event

--- utils/filter-events.js
+++ utils/filter-events.js
@@ -1,7 +1,8 @@
-
+import Event from '../app/models/event.js'
 
 const filterMessage = (messages) => (
   messages.filter(message => {
+    message = new Event(message)
     if (message.isGroup) {
       ['ai ', "AI ", 'ai:', 'AI:'].filter(word => message.text.startsWith(word)).length > 0
     } else {
