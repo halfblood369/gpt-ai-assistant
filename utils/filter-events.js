b4133021619e53382c62cbac7e193193c226a32e
halfblood369 <halfblood369@gmail.com>
2024-08-19 17:51:11 +0800

    gpt 的前缀

--- utils/filter-events.js
+++ utils/filter-events.js
@@ -1,10 +1,11 @@
 import Event from '../app/models/event.js'
 
+// 过滤客户端的消息，对群消息有选择的回复
 const filterMessage = (messages) => (
   messages.filter(message => {
     message = new Event(message)
     if (message.isGroup) {
-      return ['ai ', "AI ", 'ai:', 'AI:'].filter(word => message.text.startsWith(word)).length > 0
+      return ['ai', 'gpt', 'gpt:', 'ai:'].filter(word => message.text.toLowerCase().startsWith(word)).length > 0
     } else {
       return true 
     }
